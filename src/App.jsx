2e71f5c88899461284280351b53fd5c08f839225
AsherOkoye <asher04od@gmail.com>
2022-07-01 21:03:25 +0100

    WavePortal Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import { ethers } from "ethers";
+import abi from "./utils/WavePortal.json";
 import './App.css'
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
   const [currentAccount, setCurrentAccount] = useState("");
+  const contractAddress = "0x61BCB7afe8CF8401D2312F849409d9C66B353742";
+  /**
+   * Create a variable here that references the abi content!
+   */
+  const contractABI = abi.abi;
 
   const checkIfWalletIsConnected = async () => {
     try {
@@ -50,14 +58,40 @@ function App() {
       console.log(error)
     }
   }
+  const wave = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        let count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+
+        /*
+        * Execute the actual wave from your smart contract
+        */
+        const waveTxn = await wavePortalContract.wave();
+        console.log("Mining...", waveTxn.hash);
+
+        await waveTxn.wait();
+        console.log("Mined -- ", waveTxn.hash);
+
+        count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   useEffect(() => {
     checkIfWalletIsConnected();
   }, [])
-
- const wave = () => {
-    
-  }
   return (
     <>
       <div className="hero">
@@ -86,14 +120,15 @@ function App() {
                   waiting on hardhat
                 </div>
               </div>
-              <div className="buttonWrap"  onClick={null}>
-                <input type="button" value="Click to Wave👋" />
+              <div style={{display:'flex'}} className="buttonWrap"  onClick={wave}>
                 {/*
         * If there is no currentAccount render this button
         */}
                 {!currentAccount && (
-                  <input type="button" value="Connect Wallet" className="waveButton" onClick={connectWallet} />
+                  <input style={{ display: 'block', }} type="button" value="Connect Wallet" className="waveButton" onClick={connectWallet} />
                 )}
+                <input style={{display: 'block', marginLeft: '50px'}} type="button" value="Click to Wave👋" />
+
               </div>
             </div>
           </div>
