308b8de4b828b913fa62d2672609bd97c6e80857
AsherOkoye <asher04od@gmail.com>
2022-07-01 20:28:44 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,42 @@ import { ethers } from "ethers";
 import './App.css'
 
 function App() {
+  /*
+  * Just a state variable we use to store our user's public wallet.
+  */
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+
+      /*
+      * Check if we're authorized to access the user's wallet
+      */
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account)
+      } else {
+        console.log("No authorized account found")
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
  const wave = () => {
     
   }
@@ -34,7 +70,7 @@ function App() {
                   waiting on hardhat
                 </div>
               </div>
-              <div className="buttonWrap"  onClick={wave}>
+              <div className="buttonWrap"  onClick={null}>
                 <input type="button" value="Click to Wave👋" />
               </div>
             </div>
